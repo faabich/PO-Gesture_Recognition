0213ff5cf6aa5cfe2d466fbae1027b26126d18fb
faabich <rostellofabian@gmail.com>
2025-04-03 10:05:14 +0200

    refractor: changed var name

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ cap = VideoCamera()
 handDetector = HandDetector(min_detection_confidence=0.7)
 
 while True:
-    status, image = cap.read()
-    handLandmarks = handDetector.findHandLandMarks(image=image, draw=True)
+    status, frame = cap.read()
+    handLandmarks = handDetector.findHandLandMarks(image=frame, draw=True)
 
     if(len(handLandmarks) != 0):
         x1, y1 = handLandmarks[4][1], handLandmarks[4][2]
@@ -24,10 +24,10 @@ while True:
         length = math.hypot(x2-x1, y2-y1)
         print(length)
 
-        cv2.circle(image, (x1, y1), 5, (255, 0, 255), cv2.FILLED)
-        cv2.circle(image, (x2, y2), 5, (255, 0, 255), cv2.FILLED)
-        cv2.line(image, (x1, y1), (x2, y2), (255, 0, 255), 3)
+        cv2.circle(frame, (x1, y1), 5, (255, 0, 255), cv2.FILLED)
+        cv2.circle(frame, (x2, y2), 5, (255, 0, 255), cv2.FILLED)
+        cv2.line(frame, (x1, y1), (x2, y2), (255, 0, 255), 3)
 
-    cv2.imshow("Volume", image)
+    cv2.imshow("Hand detector", frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
