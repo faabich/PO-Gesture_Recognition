c5bd840f0b4b5d54dd9ab46534860c63d18852e5
NathanFilipowitz <py58wpb@eduvaud.ch>
2025-04-11 14:54:10 +0200

    feat: added implementation of widgets in the main window

--- main.py
+++ main.py
@@ -6,19 +6,39 @@ Version:      0.1
 Description:  Entry point for the HandGesture application
 """
 
-import customtkinter
-import utils.HM_window
+import customtkinter as ctk
+import utils.HM_window as HM_window
 
-class App(customtkinter.CTk):
-    def __init__(self):
-        super().__init__()
-        self.geometry("400x150")
+width = 400
+height = 480
 
-        self.button = customtkinter.CTkButton(self, text="Launch main window", command=self.start_HM_window)
-        self.button.pack(padx=20, pady=20)
+app = ctk.CTk()
 
-    def start_HM_window(self):
-        new_window = HM_window.HM_window(self)
+def start_HM_window(HM_width, HM_height):
+    new_window = HM_window.HM_window(HM_width, HM_height)
 
-app = App()
+def checkbox_event():
+    pass
+
+def optionmenu_callback(choice):
+    print("optionmenu dropdown clicked:", choice)
+
+label = ctk.CTkLabel(app,text="Select a resolution for the window")
+label.grid(row=1, column=1, padx=10, pady=10)
+
+optionmenu_var = ctk.StringVar(value="option 2")
+optionmenu = ctk.CTkOptionMenu(app,values=["option 1", "option 2"],
+                                         command=optionmenu_callback,
+                                         variable=optionmenu_var)
+optionmenu.grid(row=1, column=2, padx=10, pady=10)
+
+check_var = ctk.StringVar(value="off")
+custom_res_cb = ctk.CTkCheckBox(app, text="Use a custom resolution", command=checkbox_event, variable=check_var, onvalue="on", offvalue="off")
+custom_res_cb.grid(row=2, column=1, pady=10)
+
+button = ctk.CTkButton(app, text="Launch main window", command=start_HM_window)
+button.grid(row=3, column=1, pady=10)
+
+
+app.geometry(f"{width}x{height}")
 app.mainloop()
